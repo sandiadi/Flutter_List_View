53f58c48eae5dd55cdcdd53783e4150f5083bb4e
sandiadi <pratamasandi55@gmail.com>
2023-06-15 19:12:39 +0700

    GG

---
+++ lib/main.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("List View")),
+      body: ListView(
+        children: const [
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 1"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 2"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 2"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 3"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 3"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 4"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 5"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+          ListTile(
+            leading: Icon(Icons.abc),
+            title: Text("Sensor 6"),
+          ),
+        ],
+      ),
+    );
+  }
+}
